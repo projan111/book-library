3a34fe68c9884e4fb4383a8d4a0fdb3ac659107e
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-07-22 21:08:38 +0545

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from "express"
+import globleErrorHandler from "./middlewares/globleErrorHandler";
 
 const app = express()
 app.use('/', (req, res, next) => {
   res.json({message:"Hello world!"})
+  // const error = createHttpError(400, "Something went wrong!")
+  // throw error;
+
+  
 })
+app.use(globleErrorHandler)
+
 
 export default app
