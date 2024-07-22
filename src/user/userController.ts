a918b651afd67f242309187b501a8851101cd7cb
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-07-22 22:37:05 +0545

    sign jwt token

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt"
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async(req: Request, res: Response, next: NextFunction) => {
   const {name, email, password} = req.body;
@@ -12,18 +14,23 @@ const createUser = async(req: Request, res: Response, next: NextFunction) => {
   }
   // Database call
   const user = await userModel.findOne({email})
+  
+    if(user){
+      const error = createHttpError(400, "User already exist with this email")
+      return next(error)
+    }
   // hash password
   const hashPassword = await bcrypt.hash(password,10)
-  
-
-  if(user){
-    const error = createHttpError(400, "User already exist with this email")
-    return next(error)
-  }
+  const newUser = await userModel.create({
+    name,
+    email,
+    password:hashPassword
+  })
+  // Generate JWT Token
+  const token = sign({sub: newUser._id}, config.jwtSecret as string, {expiresIn: "7d"})
   
   // Response 
-
-  res.json({message:"User Registered!"});
+  res.json({accessToken: token});
 }
 
 export default createUser;
