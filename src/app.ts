41108f1a133c9fb2bb24755218a416526fff1ed1
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-07-22 21:38:07 +0545

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express from "express"
 import globleErrorHandler from "./middlewares/globleErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express()
-app.use('/', (req, res, next) => {
+
+app.use('/home', (req, res, next) => {
   res.json({message:"Hello world!"})
   // const error = createHttpError(400, "Something went wrong!")
-  // throw error;
-
-  
+  // throw error; 
 })
+// From user Router (Express)
+app.use("/api/user", userRouter);
+// From middlewares Htttp Error handler
 app.use(globleErrorHandler)
 
 
