58ffeac76c1e513f54563b83cb2a3ec8e1d4fb61
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-07-24 15:23:01 +0545

    add multer package

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,6 +1,9 @@
 import {Request, Response, NextFunction } from "express";
 
 const createBook = (req: Request, res: Response, next: NextFunction) => {
+
+  console.log('files:', req.files)
+  
   res.status(201).json({message: "Book router is working fine"});
 }
 
